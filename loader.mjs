7f7e4a457ddcd7cf90ad8837d70b3a7efc3cfbb6
UmBa <baysal@smile-nation.de>
2026-04-16 20:28:09 +0200

    Clean: remove .mjs files, only index.html + vercel.json

--- loader.mjs
+++
@@ -1,6 +0,0 @@
-(async function(){
-  async function load(url){var r=await fetch(url);var t=await r.text();(new Function(t))();}
-  await load('/chart.dat');
-  await load('/sb.dat');
-  await load('/auth.dat');
-})();
